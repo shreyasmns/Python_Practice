6edb3d92a7c3b9d6eeb5377c023e8ae242fd330e
Shreyas MN <shreyaschinni@gmail.com>
2017-01-22 16:01:43 -0500

    Add files via upload

---
+++ list_enumeration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 15 02:22:03 2017
+
+@author: Shreyas
+"""
+
+seasons = ['Spring', 'Winter', 'Summer']
+
+list(enumerate(seasons, start=4))
+for key in seasons:
+    print(key)
